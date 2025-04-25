b76e9b6ed43ef21d56657724c43ec63a741db470
its-robii <robiulhassanrobi33@gmail.com>
2025-04-25 21:21:35 +0600

    update

--- src/pages/ResponsiveHome.jsx
+++ src/pages/ResponsiveHome.jsx
@@ -1,4 +1,3 @@
-// src/pages/ResponsiveHome.jsx
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import Sidebar from "../component/Home.jsx/Sidebar";
