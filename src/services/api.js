08c30bf4692a85894eda3931c8105ecd178595dd
its-robii <robiulhassanrobi33@gmail.com>
2025-04-28 14:35:52 +0600

    update

--- src/services/api.js
+++ src/services/api.js
@@ -1,23 +1,23 @@
-import axios from 'axios'; 
+import axios from "axios";
 
 const api = axios.create({
-    baseURL : import.meta.env.VITE_BASE_URL,
-    headers : {
-        "Content-Type" : "application/json"
-     }
-})
+  baseURL: import.meta.env.VITE_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
 export const authServices = {
-    registration : async (userData) => {
-      const res =  await api.post("/auth/registration", userData);
-        return res.data;
-    },
-    emailvariefication: async (email , otp) => {
-         const res = await api.post("/auth/emailvariefication", {email , otp});
-         return res.data
-    },
-    resentOtp : async (email) => {
-      const res = await api.post("/auth/resentotp", {email});
-      return res.data
-    }
-};  
+  registration: async (userData) => {
+    const res = await api.post("/auth/registration", userData);
+    return res.data;
+  },
+  emailvariefication: async (email, otp) => {
+    const res = await api.post("/auth/emailvariefication", { email, otp });
+    return res.data;
+  },
+  resentOtp: async (email) => {
+    const res = await api.post("/auth/resentotp", { email });
+    return res.data;
+  },
+};
