972de3a4f1e182cf1718e5d8c9f24f0031fc019a
its-robii <robiulhassanrobi33@gmail.com>
2025-05-25 21:42:34 +0600

    profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -7,18 +7,25 @@ const Profile = () => {
   return (
     <section className="h-screen w-full flex items-center justify-center px-2.5 bg-gradient-to-br from-pink-300 via-pink-200">
       <div className="ml-auto hidden md:block">
-      <Sidebar />
+        <Sidebar />
       </div>
       <div className=" relative w-full max-w-xl max-h-min flex gap-6 md:gap-8 bg-white text-black p-4 rounded-lg mx-auto shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
         <span className="absolute top-4 left-3 text-gray-600">
-          <Link to="/home" className="text-2xl"><MdArrowBackIos /></Link>
+          <Link to="/home" className="text-2xl">
+            <MdArrowBackIos />
+          </Link>
         </span>
         <div className="my-auto">
-          <img className="w-full max-w-xs h-auto sm:max-w-sm md:max-w-md  object-cover rounded-full md:rounded  border-2 border-indigo-400 shadow-lg" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjZirTv3YUaHSe-VVIQzwXUHXxb8mnJ-krbg&s"/>
+          <img
+            className="w-full max-w-xs h-auto sm:max-w-sm md:max-w-md  object-cover rounded-full md:rounded  border-2 border-indigo-400 shadow-lg"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjZirTv3YUaHSe-VVIQzwXUHXxb8mnJ-krbg&s"
+          />
         </div>
         <div className="py-2.5">
           <h3 className="text-2xl font-bold mb-2">Md Robiul</h3>
-          <p className="text-gray-400 mb-4">Full Stack Developer | React Enthusiast | UI/UX Designer</p>
+          <p className="text-gray-400 mb-4">
+            Full Stack Developer | React Enthusiast | UI/UX Designer
+          </p>
           <p className="text-gray-600 mb-4">robiuls33@gmail.com</p>
         </div>
       </div>
